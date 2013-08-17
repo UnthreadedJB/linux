c5d95f1c06b23269a24af1a5b27be9b0484311f3
#unthreadedjb <unthreadedjb@gmail.com>
2013-08-17 18:10:26 -0500

    we r of #updat #UTS

--- include/linux/uts.h
+++ include/linux/uts.h
@@ -5,7 +5,7 @@
  * Defines for what uname() should return 
  */
 #ifndef UTS_SYSNAME
-#define UTS_SYSNAME "Linux"
+#define UTS_SYSNAME "UNTHEDEURENIX PROFESIONAL KERNEL RELEES v50.00 ALAPHA"
 #endif
 
 #ifndef UTS_NODENAME
